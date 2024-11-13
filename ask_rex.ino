4a1c43b355cf1dee53237542ab8ffc3f87464703
falconghatol <128960205+falconghatol@users.noreply.github.com>
2024-11-13 22:37:05 +0530

    Update ask_rex.ino

--- ask_rex.ino
+++ ask_rex.ino
@@ -1,19 +1,10 @@
-// ask_receiver.pde
-// -*- mode: C++ -*-
-// Simple example of how to use RadioHead to receive messages
-// with a simple ASK transmitter in a very simple way.
-// Implements a simplex (one-way) receiver with an Rx-B1 module
-// Tested on Arduino Mega, Duemilanova, Uno, Due, Teensy, ESP-12
-
 #include <RH_ASK.h>
 #ifdef RH_HAVE_HARDWARE_SPI
 #include <SPI.h> // Not actually used but needed to compile
 #endif
 
-//RH_ASK driver;
-RH_ASK driver(2000, 14, 4, 0); // ESP8266 or ESP32: do not use pin 11 or 2
-// RH_ASK driver(2000, 3, 4, 0); // ATTiny, RX on D3 (pin 2 on attiny85) TX on D4 (pin 3 on attiny85), 
-// RH_ASK driver(2000, PD14, PD13, 0); STM32F4 Discovery: see tx and rx on Orange and Red LEDS
+RH_ASK driver(2000, 14, 4, 0); /
+
 
 void setup()
 {
@@ -37,8 +28,6 @@ void loop()
     {
 	int i;
 
-	// Message with a good checksum received, dump it.
-	//driver.printBuffer("Got:", buf, buflen);
   Serial.println((char*)buf);
     }
 }
